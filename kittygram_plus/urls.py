328afeb2e634aafc80c7a174ffbd33ee5cc5b281
Pavel Naumenko <pavelnaumenkowork@gmail.com>
2023-04-03 15:03:33 +0300

    auth added

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -1,5 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views
+
 
 from cats.views import CatViewSet, OwnerViewSet
 
@@ -10,4 +12,6 @@ router.register('owners', OwnerViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token),
+
 ]
